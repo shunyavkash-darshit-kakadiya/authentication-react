1d16afa21e3db4f21be80b366ab3056d3ca6b5da
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-23 18:12:16 +0530

    install @react-oauth/google and @mui/material

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { VITE_GOOGLE_CLIENT_ID } from "./constants/environment.js";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <GoogleOAuthProvider clientId={VITE_GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </>
 );
