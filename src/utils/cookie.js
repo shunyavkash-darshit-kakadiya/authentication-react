d48f5166491fbde81ef863b8d4b07a19c69da0af
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-28 20:09:31 +0530

    Update cookie.js

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -11,3 +11,17 @@ export function getCookie(name) {
   const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
   return match ? match[2] : null;
 }
+
+const VISITOR_ID_COOKIE = "x-visitor-id";
+
+// Get visitor ID from cookie
+export function getVisitorId() {
+  return getCookie(VISITOR_ID_COOKIE);
+}
+
+// Set visitor ID in cookie
+export function setVisitorId(visitorId) {
+  if (visitorId) {
+    setCookie(VISITOR_ID_COOKIE, visitorId, 15);
+  }
+}
