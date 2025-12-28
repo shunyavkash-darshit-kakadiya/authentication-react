95492cc26b4d23d320c52030a3d46619cd8e3780
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-28 20:10:00 +0530

    logout api call

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -10,16 +10,23 @@ import {
     TableContainer,
     TableHead,
     TableRow,
+    Tab,
+    Button,
+    IconButton,
 } from "@mui/material";
+import { Delete as DeleteIcon } from "@mui/icons-material";
 import apiService from "../../services/apiService";
 import apiList from "../../constants/apiList";
+import { getVisitorId } from "../../utils/cookie";
 
 const DashboardPage = () => {
     const [devices, setDevices] = useState([]);
+    const [visitorId, setVisitorId] = useState(null);
 
     useEffect(() => {
-        console.log("DashboardPage Mounted");
-    },[]);
+        const currentVisitorId = getVisitorId();
+        setVisitorId(currentVisitorId);
+    }, []);
 
     useEffect(() => {
         const fetchDevices = async () => {
@@ -36,6 +43,24 @@ const DashboardPage = () => {
         fetchDevices();
     }, []);
 
+    const deleteDevice = async (deviceId) => {
+        try {
+            const deleteUrl = apiList.ACTIVE_DEVICE.DELETE_DEVICE.url.replace(':id', deviceId);
+            const deleteConfig = {
+                ...apiList.ACTIVE_DEVICE.DELETE_DEVICE,
+                url: deleteUrl
+            };
+
+            const response = await apiService(deleteConfig);
+
+            if (response.success) {
+                setDevices(prevDevices => prevDevices.filter(device => device._id !== deviceId));
+            }
+        } catch (err) {
+            console.error("Error deleting device:", err);
+        }
+    };
+
     return (
         <Container maxWidth="lg">
             <Box mt={4}>
@@ -50,6 +75,7 @@ const DashboardPage = () => {
                                 <TableCell>OS</TableCell>
                                 <TableCell>User Agent</TableCell>
                                 <TableCell>Logged In</TableCell>
+                                <TableCell>Action</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -61,6 +87,11 @@ const DashboardPage = () => {
                                         {device.userAgent}
                                     </TableCell>
                                     <TableCell>{new Date(device.createdAt).toLocaleString()}</TableCell>
+                                    <TableCell>
+                                        <Button onClick={() => deleteDevice(device._id)} disabled={device.visitorId === visitorId} variant="contained" color="primary" size="small">
+                                            Logout
+                                        </Button>
+                                    </TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
