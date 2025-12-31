3fb01837e45b7512f8102d5fb0e792ff4e50ef36
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-31 12:51:02 +0530

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
 import js from "@eslint/js";
-import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{js,jsx}"],
+
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -23,6 +29,13 @@ export default defineConfig([
       },
     },
     rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^"]],
+        },
+      ],
+      "simple-import-sort/exports": "error",
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
     },
   },
